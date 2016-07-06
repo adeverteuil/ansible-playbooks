e563aee1cd3961e39ae82cb0fcbe26e773cf658d
Alexandre de Verteuil <alexandre@deverteuil.net>
2016-07-05 22:03:16 -0400

    Do MySQL database dumps daily.

---
+++ roles/mysql/files/mysqldump_cronjob.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Source: http://simon-davies.name/bash/backing-up-mysql-databases
+# Assuming credentials are available in ~/.my.cnf
+
+# Setup
+backup_path="/var/backups/mysql"
+date=$(date +"%Y-%m-%d_%H:%M")
+umask 177
+
+# Do the backup
+mysqldump --all-databases > "$backup_path/dump.$date.sql"
+
+# Delete files older than 30 days, making sure at least 10 backups would remain.
+if [ $(find "$backup_path" -mindepth 1 -name "dump.*.sql" -mtime -30 | wc -l) -gt 10 ]; then
+    find "$backup_path" -mindepth 1 -mtime "dump.*.sql" +30 -exec rm '{}' \;
+fi
