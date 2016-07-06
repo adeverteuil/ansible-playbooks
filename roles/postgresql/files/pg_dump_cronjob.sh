c4ee0edb2ada5be25cac967666782de94beacae1
Alexandre de Verteuil <alexandre@deverteuil.net>
2016-07-05 22:26:15 -0400

    Do daily PostgreSQL database dumps.

---
+++ roles/postgresql/files/pg_dump_cronjob.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Setup
+backup_path="/var/backups/postgresql"
+date=$(date +"%Y-%m-%d_%H:%M")
+umask 177
+
+# Do the backup
+sudo -u postgres pg_dumpall > "$backup_path/dump.$date.sql"
+
+# Delete files older than 30 days, making sure at least 10 backups would remain.
+if [ $(find "$backup_path" -mindepth 1 -name "dump.*.sql" -mtime -30 | wc -l) -gt 10 ]; then
+    find "$backup_path" -mindepth 1 -mtime "dump.*.sql" +30 -exec rm '{}' \;
+fi
