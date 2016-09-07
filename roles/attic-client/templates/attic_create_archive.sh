89dbdeaa126c71077ddad2335b5f0c1ec2dc6644
Alexandre de Verteuil <alexandre@deverteuil.net>
2016-09-06 21:28:32 -0400

    Created script attic_create_archive.sh.

---
+++ roles/attic-client/templates/attic_create_archive.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# {{ ansible_managed }}
+# The PATHS variable value is from a template variable.
+
+NICE="nice -n 19 ionice -c 2 -n 7"
+REPOSITORY="attic@attic.private.deverteuil.net:/var/lib/attic/repository"
+ARCHIVE="{{ ansible_fqdn }}-$(date -Iseconds)"
+ATTIC="attic create ${REPOSITORY}::${ARCHIVE}"
+OPTIONS="--exclude-from /etc/attic.exclude --do-not-cross-mountpoints"
+PATHS="{{ attic_backup_paths }}"
+
+(
+if ! flock -n 9; then
+  echo "Couldn't acquire lock /run/attic.lock"
+  exit 1
+fi
+stdout=$($NICE $ATTIC $OPTIONS $PATHS 2>&1) || echo $stdout
+) 9>/run/attic.lock
