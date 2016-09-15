0a6a76c88a5a24944f345ec97768afb91a726509
Alexandre de Verteuil <alexandre@deverteuil.net>
2016-09-15 00:13:05 -0400

    Added a timeout to flock command in borg_serve.sh.

--- roles/borg-server/files/borg_serve.sh
+++ roles/borg-server/files/borg_serve.sh
@@ -2,7 +2,7 @@
 
 cd /var/lib/attic
 if [ -e /var/lib/attic/repository/lock ]; then
-  flock /var/lib/attic/repository/lock borg serve --restrict-to-path /var/lib/attic --append-only
+  flock --wait 7170 /var/lib/attic/repository/lock borg serve --restrict-to-path /var/lib/attic --append-only
 else
   exit 1
 fi
