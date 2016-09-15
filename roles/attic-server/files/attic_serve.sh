480a8cad70b8fca2de26bdbfe416602558cdafd8
Alexandre de Verteuil <alexandre@deverteuil.net>
2016-09-15 00:14:15 -0400

    Removed roles attic and backup.

--- roles/attic-server/files/attic_serve.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /var/lib/attic
-if [ -e /var/lib/attic/repository/lock ]; then
-  flock /var/lib/attic/repository/lock attic serve --restrict-to-path /var/lib/attic
-else
-  exit 1
-fi
